9ac47aaf6dc682e1b72a310022fc1d08842e8104
Aleh Biahun <stones2875@gmail.com>
2024-05-22 10:09:15 +0200

    Solution to task number 8 version_3

--- HW8/Task2/src/Main.java
+++ HW8/Task2/src/Main.java
@@ -15,8 +15,28 @@ public class Main {
             weights[i] = random.nextInt((maxWeight - minWeight) + 1) + minWeight;
         }
         System.out.println("Generated weights for 10 people:");
+
         for (int weight : weights) {
             System.out.println(weight);
         }
+
+        System.out.println("*****************\n");
+
+        int averageWeight = 0;
+        for (int weight : weights) {
+            averageWeight += weight;
+        }
+        System.out.println("Average weight for 10 people: " + averageWeight / numberOfPeople + "kg");
+
+        System.out.println("\n*****************\n");
+
+        int peopleCounter = 0;
+        for (int weight : weights) {
+            if (weight >= 60 && weight <= 80) {
+                peopleCounter++;
+            }
+        }
+        System.out.println("The number of people in the range from 60kg to 80kg: " + peopleCounter);
+
     }
 }
