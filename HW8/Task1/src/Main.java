3ef29ce2be4932c6b8c7314247530ba21a53e834
Aleh Biahun <stones2875@gmail.com>
2024-05-22 09:38:47 +0200

    Solution to task number 8 version_2

--- HW8/Task1/src/Main.java
+++ HW8/Task1/src/Main.java
@@ -29,23 +29,5 @@ public class Main {
         for (int i = 0; i < array.length; i++){
             System.out.println(array[i]);
         }
-
-        System.out.println("***********************\n");
-
-        int numberOfPeople = 10;
-        int minWeight = 40;
-        int maxWeight = 100;
-
-        int[] weights = new int[numberOfPeople];
-
-        Random random = new Random();
-
-        for (int i = 0; i < numberOfPeople; i++) {
-            weights[i] = random.nextInt((maxWeight - minWeight) + 1) + minWeight;
-        }
-        System.out.println("Generated weights for 10 people:");
-        for (int weight : weights) {
-            System.out.println(weight);
-        }
     }
 }
--- HW8/Task2/src/Main.java
+++ HW8/Task2/src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        //todo код программы писать тут
+
+        int numberOfPeople = 10;
+        int minWeight = 40;
+        int maxWeight = 100;
+
+        int[] weights = new int[numberOfPeople];
+
+        Random random = new Random();
+
+        for (int i = 0; i < numberOfPeople; i++) {
+            weights[i] = random.nextInt((maxWeight - minWeight) + 1) + minWeight;
+        }
+        System.out.println("Generated weights for 10 people:");
+        for (int weight : weights) {
+            System.out.println(weight);
+        }
     }
 }
